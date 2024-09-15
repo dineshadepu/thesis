77a17b406cb94d32df28f50aea958d35d1ff0856
dineshadepu <adepu.dinesh.a@gmail.com>
2024-09-15 17:51:16 +0530

    Add presentation

---
+++ presentation/videos/convert.sh
@@ -0,0 +1,14 @@
+ffmpeg -i 10a_erosion_sphere_ductile.webm -qscale 0 10a_erosion_sphere_ductile.mp4
+ffmpeg -i 10b_erosion_sphere_ductile.webm -qscale 0 10b_erosion_sphere_ductile.mp4
+ffmpeg -i 1_dam_break_2d.webm -qscale 0 1_dam_break_2d.mp4
+ffmpeg -i 2a_oscillating_plate_etvf_ipst.webm -qscale 0 2a_oscillating_plate_etvf_ipst.mp4
+ffmpeg -i 2b_oscillating_plate_gtvf_sun.webm -qscale 0 2b_oscillating_plate_etvf_sun.mp4
+ffmpeg -i 3a_rings_etvf_sun_0_47.webm -qscale 0 g -i 3a_rings_etvf_sun_0_47.mp4
+ffmpeg -i 3b_rings_etvf_ipst_0_47.webm -qscale 0  -i 3b_rings_etvf_ipst_0_47.mp4
+ffmpeg -i 4a_dinesh_elastic_solids_passing_by_ctvf.webm -qscale 0 stic_solids_passing_by_ctvf.mp4
+ffmpeg -i 4b_dinesh_elastic_solids_passing_by_mohseni.webm -qscale 0 c_solids_passing_by_mohseni.mp4
+ffmpeg -i 5_stress_wave_propagation_part_2.webm -qscale 0 stress_wave_propagation_part_2.mp4
+ffmpeg -i 6_mohseni_free_sliding_2d_rb.webm -qscale 0 _mohseni_free_sliding_2d_rb.mp4
+ffmpeg -i 7_stack_of_cylinders.webm -qscale 0 7_stack_of_cylinders.mp4
+ffmpeg -i 8_rigid_fluid_coupling_cylinder_sink.webm -qscale 0 luid_coupling_cylinder_sink.mp4
+ffmpeg -i fsi.webm -qscale 0 9_fsi.mp4
